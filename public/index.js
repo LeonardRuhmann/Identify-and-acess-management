eb8d922da7afab0034c599835867e52bfd5c6a47
Leonardo Rühmann <leonardoruhmannwork@gmail.com>
2024-05-15 21:45:11 -0300

    feat: creating the user authetication in login.html

---
+++ public/index.js
@@ -0,0 +1,24 @@
+function loginButton() {
+  let user = document.getElementById("user").value;
+  let password = document.getElementById("password").value;
+  fetch("http://localhost:8000/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const userExist = data.some(
+        (item) => item.user === user && item.password === password
+      );
+      if (userExist) {
+        alert("Usuário autenticado com sucesso!");
+      } else {
+        alert("Usuário não existe!");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
