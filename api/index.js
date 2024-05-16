023ce6a44c15db4292cb025c9f8b38f2fa9213cc
Leonardo Rühmann <leonardoruhmannwork@gmail.com>
2024-05-16 13:05:49 -0300

    feat: adding the CORS

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const UserRoutes = require("./src/router/userRoutes.js");
 const app = express();
 const port = 8000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use("/", UserRoutes);
 
