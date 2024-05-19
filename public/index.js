0c212a43985bcefc96996cacfa866b2111a49ac7
Leonardo Rühmann <leonardoruhmannwork@gmail.com>
2024-05-19 19:22:42 -0300

    feat: add the possibility to upload a profile image

--- public/index.js
+++ public/index.js
@@ -22,3 +22,21 @@ function loginButton() {
       console.error(error);
     });
 }
+
+const inputFile = document
+  .getElementById("image-profile")
+  .addEventListener("click", () => {
+    document.getElementById("file").click();
+  });
+
+document.getElementById("file").addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    document.getElementById("image-profile").src = reader.result;
+  };
+  console.log(reader);
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+});
