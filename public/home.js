fa2af1ec65a233ae42c4e79d0d2b061125fabe0b
Leonardo Rühmann <leonardoruhmannwork@gmail.com>
2024-06-03 02:35:21 -0300

    feat: render the user datas in home page

---
+++ public/home.js
@@ -0,0 +1,41 @@
+import { fetchGet } from "./scripts/fetchGet.js";
+
+async function findUser() {
+  const id = getUrlParamId();
+  const allUsers = await fetchGet();
+
+  const user = allUsers.find((user) => user._id === id);
+  return user;
+}
+
+function getUrlParamId() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get("userId");
+  return id;
+}
+
+async function renderUser() {
+  const user = await findUser();
+
+  const inputs = getInputsArea();
+
+  inputs.nameInput.value = user.user;
+  inputs.officeInput.value = user.group;
+  inputs.birthdayInput.value = user.birthday;
+  inputs.groupInput.value = user.group;
+  inputs.imageProfile.forEach(
+    (image) => (image.src = `data:image/jpeg;base64, ${user.image}`)
+  );
+}
+
+function getInputsArea() {
+  const nameInput = document.getElementById("nameInput");
+  const officeInput = document.getElementById("officeInput");
+  const birthdayInput = document.getElementById("birthdayInput");
+  const groupInput = document.getElementById("groupInput");
+  const imageProfile = document.querySelectorAll(".image-profile");
+
+  return { nameInput, officeInput, birthdayInput, groupInput, imageProfile };
+}
+
+renderUser();
